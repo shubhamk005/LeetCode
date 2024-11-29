34723f8f05738eab23c9cf6178c292e90519dff9
shubhamk005 <149696129+shubhamk005@users.noreply.github.com>
2024-11-29 18:39:59 +0530

    Update 0067-Add Binary.cpp
    leading zeros handled\

--- 0001-0100/0067-Add Binary/0067-Add Binary.cpp
+++ 0001-0100/0067-Add Binary/0067-Add Binary.cpp
@@ -1,8 +1,30 @@
 class Solution {
-public:
+  public:
     string addBinary(string a, string b) {
         string result;
         int i = a.size() - 1, j = b.size() - 1, carry = 0;
+        reverse(a.begin(), a.end());
+        reverse(b.begin(), b.end());
+        for(int m=i;m>=0;m--){
+            if(a[m]=='0'){
+                a.pop_back();
+            }
+            else{
+                break;
+            }
+        }
+        for(int n=j;n>=0;n--){
+            if(b[n]=='0'){
+                b.pop_back();
+            }
+            else{
+                break;
+            }
+        }
+        i=a.size()-1;
+        j=b.size()-1;
+        reverse(a.begin(), a.end());
+        reverse(b.begin(), b.end());
         while (i >= 0 || j >= 0 || carry) {
             int sum = carry;
             if (i >= 0) {
@@ -11,10 +33,9 @@ public:
             if (j >= 0) {
                 sum += b[j--] - '0';
             }
-            result += '0' + (sum & 1);
+           result += (sum % 2) ? '1' : '0';
             carry = sum / 2;
         }
-        
         reverse(result.begin(), result.end());        
         return result;
     }
